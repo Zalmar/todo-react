c7b4cd58acb8b8d703464dacc10baa0edd202e2f
Zalmar <zalmardev@gmail.com>
2019-11-17 09:10:53 +0400

    Create components AppHeader, SearchPanel, TodoList and TodoListItem

---
+++ src/components/search-panel.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchPanel = () => {
+    return <input placeholder="search"></input>
+};
+
+export default SearchPanel;
---
+++ src/components/todo-list-item.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TodoListItem = () => {
+    return <span>Drink Tea</span>
+};
+
+export default TodoListItem;
